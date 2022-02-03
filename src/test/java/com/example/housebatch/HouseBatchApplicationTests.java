88d67fd09f167878a5a97a3075b809671dd02f61
kjaehyeon <jacob9404@gmail.com>
2022-02-03 11:37:47 +0900

    Lawd Job Batch complete

--- src/test/java/com/example/housebatch/HouseBatchApplicationTests.java
+++ src/test/java/com/example/housebatch/HouseBatchApplicationTests.java
@@ -3,6 +3,7 @@ package com.example.housebatch;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
 @SpringBootTest
 class HouseBatchApplicationTests {
 
