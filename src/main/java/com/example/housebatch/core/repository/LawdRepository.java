339058958b3078eb1efe26e2f5de6f5387a083ad
kjaehyeon <jacob9404@gmail.com>
2022-02-05 23:58:48 +0900

    get gu code from db and pass them through JobexecutionContext

--- src/main/java/com/example/housebatch/core/repository/LawdRepository.java
+++ src/main/java/com/example/housebatch/core/repository/LawdRepository.java
@@ -2,9 +2,17 @@ package com.example.housebatch.core.repository;
 
 import com.example.housebatch.core.entity.Lawd;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface LawdRepository extends JpaRepository<Lawd, Long> {
     Optional<Lawd> findByLawdCd(String lawdCd);
+
+    @Query("select distinct substring(l.lawdCd, 1, 5) " +
+            "from Lawd l " +
+            "where l.exist = true " +
+            "and l.lawdCd not like '%00000000'")
+    List<String> findDistinctGuLawdCd();
 }
--- src/main/java/com/example/housebatch/job/apt/AptDealInsertJobConfig.java
+++ src/main/java/com/example/housebatch/job/apt/AptDealInsertJobConfig.java
@@ -2,31 +2,33 @@ package com.example.housebatch.job.apt;
 
 import com.example.housebatch.adapter.ApartmentApiResource;
 import com.example.housebatch.core.dto.AptDealDto;
-import com.example.housebatch.job.validator.FilePathParameterValidator;
+import com.example.housebatch.core.repository.LawdRepository;
 import com.example.housebatch.job.validator.LawdCdParameterValidator;
 import com.example.housebatch.job.validator.YearMonthParameterValidator;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobParametersValidator;
-import org.springframework.batch.core.Step;
+import org.springframework.batch.core.*;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.job.CompositeJobParametersValidator;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.item.ExecutionContext;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.xml.StaxEventItemReader;
 import org.springframework.batch.item.xml.builder.StaxEventItemReaderBuilder;
+import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 
 import java.time.YearMonth;
 import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @RequiredArgsConstructor
@@ -35,13 +37,18 @@ public class AptDealInsertJobConfig {
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
     private final ApartmentApiResource apartmentApiResource;
+    private final LawdRepository lawdRepository;
 
     @Bean
-    public Job aptDealInsertJob(Step aptDealInsertStep){
+    public Job aptDealInsertJob(
+            Step aptDealInsertStep,
+            Step guLawdCdStep
+    ){
         return jobBuilderFactory.get("aptDealInsertJob")
                 .incrementer(new RunIdIncrementer())
                 .validator(aptDealJobParameterValidator())
-                .start(aptDealInsertStep)
+                .start(guLawdCdStep)
+                .next(aptDealInsertStep)
                 .build();
     }
 
@@ -49,10 +56,32 @@ public class AptDealInsertJobConfig {
         //validator 여러개를 합쳐준다.
         CompositeJobParametersValidator validator = new CompositeJobParametersValidator();
         validator.setValidators(Arrays.asList(
-                new YearMonthParameterValidator(),
-                new LawdCdParameterValidator()
+                //new LawdCdParameterValidator(),
+                new YearMonthParameterValidator()
         ));
         return validator;
+    }
+    @JobScope
+    @Bean
+    public Step guLawdCdStep(Tasklet guLawdCdTasklet){
+        return stepBuilderFactory.get("guLawdCdStep")
+                .tasklet(guLawdCdTasklet)
+                .build();
+    }
+
+    @StepScope
+    @Bean
+    public Tasklet guLawdCdTasklet(){
+        return (contribution, chunkContext) -> {
+            StepExecution stepExecution = chunkContext.getStepContext().getStepExecution();
+            ExecutionContext executionContext = stepExecution.getJobExecution().getExecutionContext();
+            //step간에 데이터를 전달할거라서 JobExcutionContext를 꺼내온다.
+
+            List<String> guLawdCds = lawdRepository.findDistinctGuLawdCd();
+            executionContext.putString("guLawdCd", guLawdCds.get(0));
+
+            return RepeatStatus.FINISHED;
+        };
     }
 
     @JobScope
@@ -74,7 +103,7 @@ public class AptDealInsertJobConfig {
     public StaxEventItemReader<AptDealDto> aptDealResourceReader(
             Jaxb2Marshaller aptDealDtoMarshaller,
             @Value("#{jobParameters['yearMonth']}") String yearMonth,
-            @Value("#{jobParameters['lawdCd']}") String lawdCd
+            @Value("#{jobExecutionContext['guLawdCd']}") String lawdCd
     ){
         return new StaxEventItemReaderBuilder<AptDealDto>()
                 .name("aptDealResourceReader")
